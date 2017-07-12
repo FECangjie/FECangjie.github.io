fc2ca6e5ed7682c9977853c918f0754d25cbcac6
QinChao <qinchao@lianjia.com>
2017-07-12 16:39:55 +0800

    嵌套模版

---
+++ src/modules/leftAction/index.js
@@ -0,0 +1,17 @@
+/**
+ *  === page ===
+ *
+ *  created at: Tue Jun 27 2017 18:27:29 GMT+0800 (CST)
+ */
+
+import { React, Page } from 'zola'
+import Tpl from './tpl.rt'
+import './style.less'
+
+export default class Index extends Page {
+  componentWillMount () {
+  }
+  render () {
+    return Tpl.apply(this)
+  }
+}
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -7,9 +7,54 @@
 import { React, Page } from 'zola'
 import Tpl from './tpl.rt'
 import './style.less'
+import LeftMenu from 'modules/leftMenu'
+import LeftAction from 'modules/leftAction'
+import articles from 'data/article'
+import AsyncComponent from 'modules/AsyncComponent'
+
+const articleList = MY_ARTICLE_DATA
 
 export default class Index extends Page {
   render () {
-    return Tpl.apply(this)
+    return (
+      <div className="container clearfix">
+      <LeftMenu></LeftMenu>
+      <LeftAction></LeftAction>
+      <div id="main" className="site-main" style={{minHeight: 382}}>
+          <div className="site-main-inner">
+              <div className="container clearfix">
+                  <div id="primary" className="content-area">
+                      <div className="primary-inner">
+                          <div id="content" className="site-content content-list" role="main">
+                          {
+                		      	articleList.map((article,index) =>{
+                		      		return (
+                                <article key={index} id="post-33" className="post-33 post type-post status-publish format-standard has-post-thumbnail hentry category-delicious category-freelancing category-photography tag-playing tag-shopping">
+                                    <header className="entry-header">
+                                        <h2 className="entry-title"><a href="" rel="bookmark">{article.title}</a></h2>
+                                        <div className="entry-meta">
+                                        <span className="byline">By <span className="author vcard"><a className="url fn n" href="" title="View all posts by Ruth Clem">Ruth Clem</a></span></span><span className="cat-links"> in <a href="" rel="category tag">Delicious</a>, <a href="" rel="category tag">Freelancing</a>, <a href="http://demo.designwall.com/dw-minion/category/photography/" rel="category tag">Photography</a></span>
+                                            <span className="sep"><span className="post-format"><i className="icon-file-text"></i></span></span><span className="posted-on"><a href="http://demo.designwall.com/dw-minion/2013/06/20/the-inside-secrets-of-millionaires-under-the-age-of-29/" title="12:00 am" rel="bookmark"><i className="icon-calendar-empty"></i> </a></span>
+                                            <span className="comments-link"><a href="http://demo.designwall.com/dw-minion/2013/06/20/the-inside-secrets-of-millionaires-under-the-age-of-29/#comments"><i className="icon-comment-alt"></i> 4 Comments</a></span>
+                                        </div>
+                                    </header>
+                                    <div className="entry-thumbnail">
+                                    </div>
+                                    <div className="entry-content">
+                                        <p>Quisque tincidunt metus aliquam orci tincidunt, ac sollicitudin massa lacinia. Nunc ligula enim, fringilla vitae neque vestibulum, fermentum semper tellus. Fusce magna nisi, tincidunt eu dui quis, tempus consequat ipsum.
+                                            <a href="http://demo.designwall.com/dw-minion/2013/06/20/the-inside-secrets-of-millionaires-under-the-age-of-29/" className="more-link"><span className="btn btn-small">Continue reading</span></a></p>
+                                    </div>
+                                </article>
+                	      			)
+                		      	})
+                		      }
+                  </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    )
   }
 }
