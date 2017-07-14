09aa722628721210e5618f4d86602ddbbf3a1182
QinChao <qinchao@lianjia.com>
2017-07-14 16:05:26 +0800

    markdown文本样式

--- src/data/article.js
+++ src/data/article.js
@@ -1,14 +1,15 @@
 
  	export default [
  	{
- 		"title":"揭开React风靡前端开发领域的面纱","shortContent":"[本文的重点是React受欢迎的原因，而不是与其他框架或库进行比较]:  以下是React如此迅速流行的几个原因： 使用DOM API成本较大。React使开发人员能够使用比真实浏览器更友好的虚拟浏览器。React的虚拟浏览器就像开发者和真实浏览器之间的代理一样。.React允许开发人员声明式地描述UI并给界面状态建模。这意味着开发者只需通过一...","path":"/yes-react-is-taking-over-front-end-development-the-question-is-why/translate","createTime":"","component":()=>System.import('article/yes-react-is-taking-over-front-end-development-the-question-is-why/translate.md')
+ 		"title":"translate","shortContent":"揭开React风靡前端开发领域的面纱: .png) [^本文的重点是React受欢迎的原因，而不是与其他框架或库进行比较]:  以下是React如此迅速流行的几个原因： 使用DOM API成本较大。React使开发人员能够使用比真实浏览器更友好的虚拟浏览器。React的虚拟浏览器就像开发者和真实浏览器之间的代理一样。.React允许开发人员声明式地描述UI并给界面状态建模。这意味着开发者只需通过一...","path":"/yes-react-is-taking-over-front-end-development-the-question-is-why/translate","createTime":"","component":()=>System.import('article/yes-react-is-taking-over-front-end-development-the-question-is-why/translate.md')
 
  	},
  	{
- 		"title":"CSS变量，来了？","shortContent":"简介： Web开发人员一直希望，可以在css开发中能够使用强大的变量。现在，CSS变量终于来了。在这篇文章中，我们会追忆CSS变量的历史，重要性以及应用，在这里你能够了解到如何利用CSS变量，来使CSS的开发和维护变得更快更容易 CSS变量的历史是坎坷的。该功能最初是由W3C在2012年提出，但是只有Chrome和Firefox对该特性进行了实现。 当该规范在2014年得...","path":"/日记","createTime":"","component":()=>System.import('article/日记.md')
+ 		"title":"日记","shortContent":"#CSS变量，来了？ ##简介： Web开发人员一直希望，可以在css开发中能够使用强大的变量。现在，CSS变量终于来了。在这篇文章中，我们会追忆CSS变量的历史，重要性以及应用，在这里你能够了解到如何利用CSS变量，来使CSS的开发和维护变得更快更容易 CSS变量的历史是坎坷的。该功能最初是由W3C在2012年提出，但是只有Chrome和Firefox对该特性进行了实现。 当该规范在2014年得...","path":"/日记","createTime":"","component":()=>System.import('article/日记.md')
 
  	},
  	{
  		"title":"马克飞象","shortContent":"欢迎使用马克飞象: @(示例笔记本)[马克飞象|帮助|Markdown] 马克飞象是一款专为印象笔记（Evernote）打造的Markdown编辑器，通过精心的设计与技术实现，配合印象笔记强大的存储和同步功能，带来前所未有的书写体验。特点概述： 功能丰富 ：支持高亮代码块、LaTeX 公式、流程图，本地图片以及附件上传，甚至截图粘贴，工作学习好帮手；.得心应手 ：简洁高效的编辑器，提供桌面客户端以...","path":"/马克飞象","createTime":"","component":()=>System.import('article/马克飞象.md')
 
  	}]
+ 	
